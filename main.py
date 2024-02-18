635b234d0bc23f6cc85612a3e7b831f79f651402
Zavate_Vlad <zavatevlad26@gmail.com>
2024-02-18 20:55:09 +0200

    Fixed bug where player could write the same state and beat the game

--- main.py
+++ main.py
@@ -6,36 +6,38 @@ screen.title("States Game")
 image = 'blank_states_img.gif'
 screen.addshape(image)
 data = pd.read_csv('50_states.csv')
-
 turtle.shape(image)
-vlad = turtle.Turtle()
+
+write_turtle = turtle.Turtle()
 score = 0
+exclusion_list = []
 
 
 def new_turtle(states):
-    vlad.penup()
-    vlad.hideturtle()
-    vlad.goto(state_x, state_y)
-    vlad.write(f'{states}', align='center')
+    write_turtle.penup()
+    write_turtle.hideturtle()
+    write_turtle.goto(state_x, state_y)
+    write_turtle.write(f'{states}', align='center')
 
 
 is_playing = True
 
-
 while is_playing:
-    exclusion_list = []
     if score == 0:
-        chosen_state = screen.textinput(title='Guess the State', prompt='Whats another state?').capitalize()
+        chosen_state = screen.textinput(title='Guess the State', prompt='Whats another state?').title()
     else:
-        chosen_state = screen.textinput(title='Guess the State', prompt=f'{score}/50 states discovered').capitalize()
+        chosen_state = screen.textinput(title='Guess the State', prompt=f'{score}/50 states discovered').title()
+
+        if chosen_state in exclusion_list:
+            chosen_state = screen.textinput(title='Guess the State', prompt='State already chosen!').title()
+
     for all_states in data['state']:
         if chosen_state == all_states:
             state = data[data['state'] == chosen_state]
+            exclusion_list.append(chosen_state)
             state_x = int(state['x'].values[0])
             state_y = int(state['y'].values[0])
             new_turtle(chosen_state)
             score += 1
-        else:
-            continue
 
 screen.mainloop()
